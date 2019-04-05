ffec2cb38ed372bdefa765f6a19c5817137b0fa0
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-04-05 18:51:45 -0300

    commit inicial. Exemplos de uso de Rigidbody2D, CircleCollider2D, BoxCollider2D, Physics Materials 2D e C# Scripts (exemplos básicos)

---
+++ Jogo2DIntroducao/Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject obj;
+    public Transform pos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(obj, pos);
+        }
+    }
+}

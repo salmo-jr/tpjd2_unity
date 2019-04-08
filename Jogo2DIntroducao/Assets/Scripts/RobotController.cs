9fd634e7575b1295884e1bb2c01c604316963b80
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-04-08 17:28:07 -0300

    Adição do RobotBoy e animações

---
+++ Jogo2DIntroducao/Assets/Scripts/RobotController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+    [HideInInspector] public bool viradoDireita = true;
+
+    public float forcaMovimento = 365f;
+    public float velocidadeMaxima = 5f;
+
+    private float h;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(h));
+    }
+}

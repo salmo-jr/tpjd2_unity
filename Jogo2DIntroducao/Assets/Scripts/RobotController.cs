55b291d04e01f0d065e5e17b0da35f2adefbf85c
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-04-10 17:30:16 -0300

    adição do script para movimentação do player

--- Jogo2DIntroducao/Assets/Scripts/RobotController.cs
+++ Jogo2DIntroducao/Assets/Scripts/RobotController.cs
@@ -11,17 +11,34 @@ public class RobotController : MonoBehaviour
 
     private float h;
     private Animator anim;
+    private Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         h = Input.GetAxis("Horizontal");
-        anim.SetFloat("Speed", Mathf.Abs(h));
+
+        rb2d.AddForce(new Vector2(h * velocidadeMaxima, rb2d.velocity.y));
+        //rb2d.velocity = new Vector2(h * velocidadeMaxima, rb2d.velocity.y);
+
+        anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+
+        if (h > 0 && !viradoDireita) Flip();
+        else if (h < 0 && viradoDireita) Flip();
+    }
+
+    private void Flip()
+    {
+        viradoDireita = !viradoDireita;
+        Vector3 escala = transform.localScale;
+        escala.x *= -1;
+        transform.localScale = escala;
     }
 }

173abb1186122f9836657d2b46e768c056ca7294
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-04-15 16:25:13 -0300

    Conclusão do exemplo básico

--- Jogo2DIntroducao/Assets/Scripts/Coletavel.cs
+++ Jogo2DIntroducao/Assets/Scripts/Coletavel.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Coletavel : MonoBehaviour
 {
+    public GameObject obj;
+    public float incremento = 1f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            GameController.instancia.AtualizaPontos(incremento);
+            GameController.instancia.obj = obj;
             Destroy(this.gameObject);
         }
     }
--- Jogo2DIntroducao/Assets/Scripts/GameController.cs
+++ Jogo2DIntroducao/Assets/Scripts/GameController.cs
@@ -1,11 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public static GameController instancia; // PADRAO DE PROJETO SINGLETON
+    public Text pontos; 
     public GameObject obj;
     public Transform pos;
+    public float coletaveis;
+
+    private void Awake()
+    {
+        // VERIFICA SE JA EXISTE UMA INSTANCIA
+        if (instancia == null)
+        {
+            // PROSSEGUE COM A INSTANCIA 
+            instancia = this;
+        }
+        else
+        {
+            if (instancia != this)
+            {
+                // DESCONSIDERA A SEGUNDA CHAMADA DE CRIACAO
+                Destroy(this);
+            }
+        }
+
+        coletaveis = 0;
+    }
 
     // Update is called once per frame
     void Update()
@@ -14,5 +38,12 @@ public class GameController : MonoBehaviour
         {
             Instantiate(obj, pos);
         }
+    }
+
+    public void AtualizaPontos(float inc)
+    {
+        //coletaveis = coletaveis + inc;
+        coletaveis += inc;
+        pontos.text = "Pontos: " + coletaveis;
     }
 }

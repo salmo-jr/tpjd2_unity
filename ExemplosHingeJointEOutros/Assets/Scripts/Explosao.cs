7f863c85dfa12e77134a28e7d1ae8e8800cab1d9
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-04-17 16:55:45 -0300

    exemplos de Hinge Joint, Area Effector e Point Effector

---
+++ ExemplosHingeJointEOutros/Assets/Scripts/Explosao.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosao : MonoBehaviour
+{
+    public GameObject explosao;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(explosao, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ ExemplosHingeJointEOutros/Assets/Scripts/FimExplosao.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FimExplosao : MonoBehaviour
+{
+    private Animator anim;
+    private float tempoAnimacao;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        tempoAnimacao = anim.GetCurrentAnimatorStateInfo(0).length;
+        Destroy(this.gameObject, tempoAnimacao);
+    }
+}
